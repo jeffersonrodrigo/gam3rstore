2fae5173ecfd78d779184ec93697eb8aac1b700c
Jefferson Rodrigo <jeffersonrodrigoaguiar@gmail.com>
2024-10-03 11:23:56 -0300

    Implementação das className e organização dos cards

--- src/components/produto/produtoItem.tsx
+++ src/components/produto/produtoItem.tsx
@@ -23,7 +23,15 @@ export default function ProdutoItem(props: ProdutoItemProps) {
                 fill
                 />
             </div>
-            {props.produto.nome}
+            <div className=' flex-1 flex flex-col p-5 gap-3 border-t border-white/10'>
+                <span className='text-lg font-semibold'>{props.produto.nome}</span>
+                <span className='text-sm border-b border-dashed self-start'>{produto.especificacoes.destaque}</span>
+                <div className='flex-1'></div>
+                <div className='flex flex-col'>
+                    <span className='text-sm text-gray-400 line-through'>De {props.produto.precoBase}</span>
+                    <span className='text-xl font-semibold text-emerald-400'>Por {props.produto.precoPromocional}</span>
+                </div>
+            </div>
         </Link>
     )
 }
