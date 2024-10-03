6597f4f1f95dfd621d8bd3f1d4c974e4c1e80a14
Jefferson Rodrigo <jeffersonrodrigoaguiar@gmail.com>
2024-10-03 12:12:43 -0300

    Finalização da aula 1 com organização do template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,10 @@ import { produtos } from "@/core";
 
 export default function Home() {
   return (
-    <div className="flex gap-5 flex-wrap">
-      <ProdutoItem produto={produtos[0]}/>
-      <ProdutoItem produto={produtos[8]}/>
-      <ProdutoItem produto={produtos[15]}/>
-      <ProdutoItem produto={produtos[22]}/>
+    <div className="grid grid-cols-4 gap-5 container">
+      {produtos.map((produto) => (
+        <ProdutoItem key={produto.id} produto={produto} />
+      ))}
     </div>
   );
 }
